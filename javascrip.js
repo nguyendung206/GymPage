10efdd3eb579bc7de83e38813c0bfe59a17fd86e
nguyendung206 <moto2062002@gmail.com>
2023-08-31 23:44:59 +0700

    update

--- javascrip.js
+++ javascrip.js
@@ -41,8 +41,8 @@ window.addEventListener("load", adjustNavbarDisplay);
 window.addEventListener("resize", adjustNavbarDisplay);
 
 // // Hiển thị thông báo đăng kí thành công
-var registerForm = document.getElementById('register-form'); // Select the form element
-var registerInputs = document.querySelectorAll('.register-input'); // Select all input fields
+var registerForm = document.getElementById('register-form'); 
+var registerInputs = document.querySelectorAll('.register-input'); 
 var registerSubmitButton = document.querySelector('.register-submit');
 var registerSuccess = document.querySelector('.register-success');
 
@@ -76,6 +76,8 @@ var buyBtns = document.querySelectorAll('.js-buy-ticket')
 var modal = document.querySelector('.modal')
 var modalClose = document.querySelector('.modal-close')
 var modalContainer = document.querySelector('.modal-container')
+var btnRegister = document.querySelector('.btn-register')
+var btnRed = document.querySelector('.btn-red')
 // Hàm mở modal buy ticket
 function showBuytickets() {
     modal.classList.add('open')
@@ -83,12 +85,52 @@ function showBuytickets() {
 // Hàm đóng modal buy ticket
 function hideBuytickets() {
     modal.classList.remove('open')
+    modalForm.style.display = 'block';
+    buySuccess.style.display = 'none';
 }
 for (var buyBtn of buyBtns) {
     buyBtn.addEventListener('click', showBuytickets)
 }
+btnRed.addEventListener('click', showBuytickets)
+btnRegister.addEventListener('click', showBuytickets)
 modalClose.addEventListener('click', hideBuytickets)
 modal.addEventListener('click', hideBuytickets)
 modalContainer.addEventListener('click', function (event) {
     event.stopPropagation()
 })
+
+var buySuccess = document.querySelector('.buy-success');
+var modalForm = document.querySelector('.modal-form');
+var buyTicket = document.querySelector('#buy-ticket');
+var buyFailure = document.querySelector('.buy-failure')
+buyTicket.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    var nameInput = document.getElementById('ticket-name');
+    var emailInput = document.getElementById('ticket-email');
+    var phoneInput = document.getElementById('ticket-phone');
+    var allInputsFilledAndValid = true;
+    // Kiểm tra input đã được điền chưa và có đúng type không
+    if (nameInput.value.trim() === '' || !nameInput.checkValidity()) {
+        allInputsFilledAndValid = false;
+    }
+
+    if (emailInput.value.trim() === '' || !emailInput.checkValidity()) {
+        allInputsFilledAndValid = false;
+    }
+
+    if (phoneInput.value.trim() === '' || !phoneInput.checkValidity()) {
+        allInputsFilledAndValid = false;
+    }
+
+    if (!allInputsFilledAndValid) {
+        event.preventDefault();
+        // Thông báo điền đầy đủ thông tin
+        buyFailure.style.display = 'block'
+    } else {
+        // Ẩn biểu mẫu và hiển thị thông báo thành công
+        modalForm.style.display = 'none';
+        buySuccess.style.display = 'block';
+        buyFailure.style.display = 'none'
+    }
+});
